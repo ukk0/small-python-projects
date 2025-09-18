ab00a9ca87f0fb9048bfd2c0ce77b10901db0105
ukk0 <sami.ukk91@gmail.com>
2025-09-18 20:51:16 +0200

    file name change toggler

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Small-Python-projects
-Relatively small-scale Python projects, mainly meant for rehearsing some concepts or for learning something new.
+Relatively small Python projects and scripts, mainly meant for rehearsing some concepts or for learning something new.
 
 - Scientific calculator: A generic scientific calculator app.
 - PDF to speech converter: Some basic tkinter and threading, uses pyttsx3 for text-to-speech.
-
---
+++ small_scripts/file_batch_rename.py
@@ -0,0 +1,29 @@
+import os
+import datetime
+
+
+# Renames all .txt files in the target folder to include current datetime.
+# If already includes the date, removes it instead.
+
+def add_date_to_txt_files(directory: str):
+    today = str(datetime.date.today())
+    for file in os.listdir(directory):
+        if file.endswith(".txt"):
+            filepath = os.path.join(directory, file)
+            name, ext = os.path.splitext(file)
+
+            if name.endswith(f"-{today}"):
+                new_name = name[:-(len(today) + 1)] + ext
+                action = "removed"
+            else:
+                new_name = f"{name}-{today}{ext}"
+                action = "added"
+
+            new_path = os.path.join(directory, new_name)
+            os.rename(filepath, new_path)
+            print(f"Date {action} for file {file}. New name is {new_name}.")
+
+
+if __name__ == "__main__":
+    path = input("Target folder (absolute path): ")
+    add_date_to_txt_files(path)
